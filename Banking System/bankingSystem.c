414691809ebaaf6bbaaacab80e303cbba9319b4c
Funakoshi Silva <55293531+funakoshiapi@users.noreply.github.com>
2020-02-22 17:45:15 -0500

    reading buffer argument / error handling

--- Banking System/bankingSystem.c
+++ Banking System/bankingSystem.c
@@ -24,15 +24,19 @@ using namespace std;
 
 int readArguments(const int& argc, char* argv[]);
 bool integerCheck (string value);
+void pFlagCheck(int i , char * argv[]);
+void bFlagCheck(int i , char * argv[]);
 
 
 int BUFFER = 5 ;  ///> can't exceed 20
-int PRODUCER = 1; ///> can't exceed 10
+int PRODUCER_THREAD_COUNT = 1; ///> can't exceed 10
 
 
 int main(int argc, char* argv[])
 {
     readArguments(argc, argv);
+
+    //cout << " number of threads "<< PRODUCER_THREAD_COUNT << endl;
 
 
 
@@ -60,8 +64,7 @@ int readArguments(const int& argc, char* argv[])
 		
 	}
 
-    int lock = 1;
-
+  
     for (int i = 1; i < argc ; i++)
     {
          if (argv[1][0] != '-')
@@ -71,9 +74,97 @@ int readArguments(const int& argc, char* argv[])
              cout<< endl;
              return 0;
          }
-         if (lock == 1) /// allows to only visit each main if statment at a time, and react to each argument value one at a time
+         if (i == 1) /// allows to only visit each main if statment at a time, and react to each argument value one at a time
          {
-                        /// strings are characters so  argv[1][0] means the first character in the second string. ///
+             pFlagCheck(i, argv); /// check if the -p flag is formated correctly
+         }
+
+         if (i == 2) /// Process second argument. The integer amout for the number of producer threads
+         {
+
+                          
+                    if (argc >= 3) // argv[2] -> interger number of producer threads
+                    {
+                    
+                        if ( integerCheck(argv[i]) ) /// check if string 2 is a interger number
+                        {
+                            int userInput = stoi(argv[i]);
+                            if(userInput > 10 || userInput < 1 )
+                            {
+                                cout << endl;
+                                cout<<" Error: Producer thread cannot be greater then 10 nor less then 1. (1-10)" << endl;
+                                
+                            }
+                            else
+                            {
+                                /// store value of procucer thread
+                                PRODUCER_THREAD_COUNT = userInput;
+                            } 
+                        }
+                        else
+                        {   cout << endl;
+                            cout << "Error: -p flag needs to be followed by interger number between 1 and 10"<< endl;
+                            cout << endl;
+                        }
+                        
+                        
+                    }
+         }
+
+        if (i == 3) /// Process third argument. Buffer flag
+        {
+               bFlagCheck(i , argv); /// check if the -b flag is formated correctly
+        }
+
+         
+        if (i == 4) /// Process fourth argument. Buffer value
+        {
+                if ( integerCheck(argv[i]) ) /// check if string 4 is a interger number
+                {
+                    int userInput = stoi(argv[i]);
+                    if(userInput > 20 || userInput < 5 )
+                    {
+                        cout << endl;
+                        cout<<" Error: Buffer cannot be greater then 20 nor less then 5. (5-20)" << endl;
+                        
+                    }
+                    else
+                    {
+                        /// store value of procucer thread
+                        BUFFER = userInput;
+                    } 
+                }
+                else
+                {   cout << endl;
+                    cout << "Error: -b flag needs to be followed by interger number between 5 and 20"<< endl;
+                    cout << endl;
+                }
+
+        }
+
+
+    }
+
+}
+
+bool integerCheck (string value)
+{
+    for( int i = 0; i < value.length(); i++)
+    {
+        if (isdigit(value[i]) == false )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+void pFlagCheck(int i , char * argv[])
+{
+
+                         /// strings are characters so  argv[1][0] means the first character in the second string. ///
                 if (argv[i][0] == '-') 
                 {
                     try
@@ -85,7 +176,7 @@ int readArguments(const int& argc, char* argv[])
                             throw message;
                         }
                     
-                        if (strlen(argv[1]) > 2)
+                        if (strlen(argv[i]) > 2) /// Reminder : i makes reference to the what string is being processed.
                         {   
                             cout<< endl;
                             cout<< "Invalid Flag: " << argv[1]<< " is not supported" << endl;
@@ -102,55 +193,52 @@ int readArguments(const int& argc, char* argv[])
                     }
                 
                 }
-         }
 
-         if (lock == 2) /// Process second argument. The integer amout for the number of producer threads
-         {
-
-                          
-                    if (argc >= 3) // argv[2] -> interger number of producer threads
-                    {
-                    
-                        if ( integerCheck(argv[2]) ) /// check if string 2 is a interger number
-                        {
-                            int userInput = stoi(argv[2]);
-                            if(userInput > 10 || userInput < 1 )
-                            {
-                                cout << endl;
-                                cout<<" Error: Producer thread cannot be greater then 10 nor less then 1. (1-10)" << endl;
-                                
-                            } 
-                        }
-                        else
-                        {   cout << endl;
-                            cout << "Error: -p flag needs to be followed by interger number between 1 and 10"<< endl;
-                            cout << endl;
-                        }
-                        
-                        
-                    }
-
-
-         }
-
-        lock += 1;
-    }
 
 
 }
 
-bool integerCheck (string value)
+
+void bFlagCheck(int i , char * argv[])
 {
-    for( int i = 0; i < value.length(); i++)
-    {
-        if (isdigit(value[i]) == false )
-        {
-            return false;
-        }
-    }
 
-    return true;
+                /// strings are characters so  argv[1][0] means the first character in the second string. ///
+                if (argv[i][0] == '-') 
+                {
+                    try
+                    {
+                        if (argv[i][1] != 'b')
+                        {
+                            /// later update to display the actual user input
+                            string message = " Error, unsupported flag. Use '-b' to set the number of producer threads ";
+                            throw message;
+                        }
+                    
+                        if (strlen(argv[i]) > 2) /// Reminder : i makes reference to the what string is being processed.
+                        {   
+                            cout<< endl;
+                            cout<< "Invalid Flag: " << argv[i]<< " is not supported" << endl;
+                            cout << "Use '-b' to set the buffer size "<< endl;
+                            cout<< endl;
+                        }
+            
+                    }
+                    catch( string message) /// first flag was not -b
+                    {   
+                        cout<< endl;
+                        cout<< message << endl;
+                        cout<< endl;
+                    }
+                
+                }
+
+
+
 }
+
+
+
+
 
 
 
